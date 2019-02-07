0b721bd4b915080c088883116645149fbab0fb74
laidasan <tp6m4xup6tp6@gmail.com>
2019-02-07 11:10:39 +0800

    DOM Javascript Practice

---
+++ JavaScriptPractice/Pracitce20190206_Document/app.js
@@ -0,0 +1,27 @@
+// let val;
+
+// val =document;
+// val = document.all;
+// val = document.all[1];
+// // val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.domain;
+// val = document.URL;
+// console.log(val);
+// console.log(typeof val);
+
+
+// let scripts = document.scripts;
+// let scriptarr = Array.from(scripts);
+// scriptarr.forEach(function(script) {
+//     console.log(script);
+// })
+// console.log(scripts);
+// console.log(typeof scripts);
+
+//DOM SELECTOR 
+console.log(document.getElementById('title'));
+
+
+console.log(document.querySelector('#title'));
