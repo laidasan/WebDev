7646b8ea4b774f8433fbfd94ba0c42a7f8d7e2b6
laidasan <tp6m4xup6tp6@gmail.com>
2019-02-08 09:57:51 +0800

    Htaml+css Heart & Some Javascript Practice

--- JavaScriptPractice/Pracitce20190206_Document/app.js
+++ JavaScriptPractice/Pracitce20190206_Document/app.js
@@ -20,8 +20,78 @@
 // console.log(scripts);
 // console.log(typeof scripts);
 
+let element = document.getElementById('title');
+
 //DOM SELECTOR 
-console.log(document.getElementById('title'));
+console.log(document.getElementById('profile-card')  );
+/* 這邊要注意，這邊會發現會有null的問題，明明已經命名了但找的時候還是return null*/
+/* 那是因為就像城市進入點一行一行往下讀一樣，看看是不是script先跑了才跑到(我們要找的Element)*/
+/* 像這裡在head裡面也有連接到這個js檔，但Element在head裡面的script之後，所以會找不到(return null)*/
+console.log(document.getElementById('profile-card').id);
+console.log(document.getElementById('profile-card').className);
+document.getElementById('img').style.background = '#fff';
+document.getElementById('img').style.color = 'black';
+// console.log(document.querySelector('#title'));
+
+//Change content
+document.getElementById('title').textContent = 'Test';
+element.innerText = 'Test2';
+element.innerHTML = 'Test3';
+
+// let eleinnerhtml = element.innerHTML;
+// console.log(eleinnerhtml);
+// console.log(typeof eleinnerhtml);
+
+// element.innerHTML = `<span style="color: red;"> Practice </span>`;
+// console.log(document.querySelector('#title').nodeName);
+// console.log(document.querySelector('#title').className);
+// // console.log(document.querySelector('.footer'));
+// console.log(document.querySelector('div'));
+// console.log(document.querySelector('td'));
+// document.querySelector('td').style.color = 'red';
+// document.querySelector('tr td').style.color = 'blue';
+// document.querySelector('td:last-child').style.color = '#fff';
+// console.log(document.querySelector('td:last-child'));
+// console.log(document.querySelector('tr:last-child'));
+// document.querySelector('tr:last-child').style.background = '#fff';
+// console.log(document.querySelector('tr:nth-child(2)'));
+// document.querySelector('tr:nth-child(2)').style.background = 'blue';
+// console.log(document.querySelector('tr:nth-child(2) td'));
+// document.querySelector('tr:nth-child(2) td').style.color = '#fff';
+// document.querySelector('tr:nth-child(2) td:last-child').style.color = '#fff';
+// document.querySelector('tr:nth-child(2) td:last-child').innerHTML = '#fff';
 
 
-console.log(document.querySelector('#title'));
+//GET BY ClassName
+const items = document.getElementsByClassName('l-gray');
+console.log(items);
+console.log(typeof items);
+console.log(items[2]);  
+items[0].style.color = '#fff';
+const table = document.querySelector('table').getElementsByClassName('l-gray');
+console.log(table);
+console.log(typeof table);
+console.log(table[0]);
+
+//document.getElementsByTagName
+const lis = document.getElementsByTagName('td');
+console.log(lis);
+console.log(typeof lis);
+let lisarrs = Array.from(lis);
+console.log(lisarrs);
+// console.log(typeof lisarrs);    //OBJECT
+console.log(lis[2]);  
+lis[2].style.color = '#fff';
+
+// lis.reverse();
+// lisarrs.reverse();  
+console.log(lisarrs);
+
+// lisarrs.forEach(function(td) {
+//     console.log(td);
+//     console.log(td.className);
+// })
+lisarrs.forEach(function(td,index) {
+    console.log(td.className);
+    td.innerHTML = `${index} : td`;
+})
