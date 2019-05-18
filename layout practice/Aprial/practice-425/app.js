b66d0ffce23096fca426c71abfe3dd62a0e5b2df
laidasan <tp6m4xup6tp6@gmail.com>
2019-05-18 17:36:21 +0800

    update about.html

--- layout practice/Aprial/practice-425/app.js
+++ layout practice/Aprial/practice-425/app.js
@@ -1,22 +1,22 @@
-{
-    // let body = document.querySelector('#body');
-    // body.innerHTML = '';
+// {
+// let body = document.querySelector('#body');
+// body.innerHTML = '';
 
-    // let wrap = document.createElement('div');
-    // body.appendChild(wrap);
-    // console.log(body);
+// let wrap = document.createElement('div');
+// body.appendChild(wrap);
+// console.log(body);
 
-    // let str = '<h1 class="heading-primary">Browser Window:</h1>' +
-    //           '<h2 class="heading-secondary">Width:' + window.innerWidth + '</h1>' + 
-    //           '<h2 class="heading-secondary">Height:' + window.innerHeight + '</h1>' + 
-    //           '<h1 class="heading-primary">Screen Window</h1>' + 
-    //           '<h2 class="heading-secondary">Width:' + screen.width+ '</h2>' + 
-    //           '<h2 class="heading-secondary">Height:' + screen.height + '</h2>'; 
-    //  let str = '<h1 class="heading-primary">Window:' + window.outerWidth + '</h1>'; 
-    // let div = body.firstChild;
-    // div.innerHTML = str;    
-    // console.log(div);
-}
+// let str = '<h1 class="heading-primary">Browser Window:</h1>' +
+//           '<h2 class="heading-secondary">Width:' + window.innerWidth + '</h1>' + 
+//           '<h2 class="heading-secondary">Height:' + window.innerHeight + '</h1>' + 
+//           '<h1 class="heading-primary">Screen Window</h1>' + 
+//           '<h2 class="heading-secondary">Width:' + screen.width+ '</h2>' + 
+//           '<h2 class="heading-secondary">Height:' + screen.height + '</h2>'; 
+//  let str = '<h1 class="heading-primary">Window:' + window.outerWidth + '</h1>'; 
+// let div = body.firstChild;
+// div.innerHTML = str;    
+// console.log(div);
+// }
 
 // let nums = [43,22,34,55,61,122];
 // nums.splice(1,4);
@@ -30,3 +30,25 @@
 // })
 // console.log(nums);
 // console.log(nums.length);
+
+
+{
+    let href = window.location.href;
+    let hrefsplit = href.split('/');
+    let nowLocation = hrefsplit[hrefsplit.length - 1];
+
+    let navLinks = document.querySelectorAll('.navigation__link');
+
+    function getNowLink() {
+        switch (nowLocation) {
+            case 'index.html':
+                navLinks[0].setAttribute('style','color:#ffffff;');
+                break;
+            case 'about.html':
+                navLinks[1].setAttribute('style', 'color: #ffffff;');
+                break;
+        }
+    }
+
+    getNowLink();
+}
