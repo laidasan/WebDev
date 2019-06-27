5c54548c3632867be4c8e01acb4f243835c3c27f
laidasan <tp6m4xup6tp6@gmail.com>
2019-06-27 12:36:19 +0800

    doc note about event(e)

---
+++ Blog/note-page/javascript/app/event_2.js
@@ -0,0 +1,73 @@
+/*     event    */
+let button = document.querySelector('.button');
+button.addEventListener('click',function(e) {
+    console.log(e);
+});
+
+//
+// type 觸發事件的名稱
+// target 觸發事件的元素
+// bubbles 是否在冒泡階段觸發
+//etc...
+
+/*    stop.Propagation()    */
+//阻擋事件冒泡
+
+let btn = document.querySelector('.btn');
+
+
+/*    e.preventDefault()    */
+//阻擋預設事件
+btn.addEventListener('click',function(e) {
+    // e.preventDefault();
+    console.log('Go To Google');
+    return false;
+})
+
+
+
+let lbl = document.querySelector('.lbl');
+let chkbox = document.querySelector('#chkbox');
+
+lbl.addEventListener('click',function() {
+    // console.log('lbl');
+})
+
+chkbox.addEventListener('click',function(e) {
+    // e.stopPropagation();
+    // console.log('chkbox');
+})
+
+
+/*    this    */
+lbl.addEventListener('click',function(e) {
+    console.log(e.target.tagName + ' 1');
+    console.log(this.tagName + ' 1');
+})
+chkbox.addEventListener('click',function(e) {
+    console.log(e.target.tagName + ' 2');
+    console.log(this.tagName + ' 2');
+})
+
+/*    事件委派    */
+let mylist = document.querySelector('.list');
+
+// if(mylist.hasChildNodes()) {
+//     for(let i =0;i < mylist.children.length;i++) {
+//         mylist.children[i].addEventListener('click',function() {
+//             console.log("I'm a item.");
+//         })
+//     }
+// }
+
+mylist.addEventListener('click',function(e) {
+    if(e.target.className === 'item') {
+        console.log("I'm a item");
+    }
+})
+let item_4 = document.createElement('li');
+item_4.className = 'item';
+item_4.textContent = 'item';
+
+mylist.appendChild(item_4);
+console.log(mylist);
