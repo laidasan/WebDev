2ee8b9bcf152293d494f93775cb73fee6b7aae75
laidasan <tp6m4xup6tp6@gmail.com>
2020-01-01 19:25:36 +0800

    ADD edit & delete & finish method

--- practice2019/41/js/app.js
+++ practice2019/41/js/app.js
@@ -2,11 +2,12 @@
     const addInput = document.querySelector('.additem');
     const notFinishedContainer = document.querySelector('.notyet');
     const finishedContainer = document.querySelector('.finish');
-    const operation = document.querySelector('.operation-container')
     const notFinishedList = document.querySelector('.notyet .list');
+    const finishedList = document.querySelector('.finish .list')
 
 
-    function newItem () {
+
+    function ListItem () {
         const item = document.createElement('li');
         item.className = 'list__item';
 
@@ -22,31 +23,72 @@
         item.appendChild(operation_container);
 
         operation_container.className = 'operation-container';
-        newEdit.className = 'operation__icon edit fas fa-pencil-alt';
-        newDelete.className = 'operation__icon delete fas fa-trash-alt';
-        newFinished.className = 'operation__icon noFinish far fa-check-circle';
-        console.log(item)
+        newEdit.className = 'operation__icon fas fa-pencil-alt';
+        newDelete.className = 'operation__icon fas fa-trash-alt';
+        newFinished.className = 'operation__icon far fa-check-circle';
+        newEdit.dataset['type'] = 'edit';
+        newDelete.dataset['type'] = 'delete';
+        newFinished.dataset['type'] = 'check';
+        newFinished.dataset['isfinish'] = '0';
         return item;
     }
 
 
     function addItem(e) {
-
         if(this.value.trim() !== '' && e.keyCode === 13) {
         const content = document.createElement('span')
         content.className = 'list__content';
         content.textContent = this.value;
-        const item = newItem();
+        const item = new ListItem();
         item.insertBefore(content,item.firstElementChild);
         notFinishedContainer.appendChild(item);
-        addInput.value = ''
+        addInput.value = '';
         }
     }
 
-    function deleteItem (e) {
+    function handleItem (e) {
         const list = e.target.parentElement.parentElement.parentElement;
-        if(e.target.className.includes('delete')) {
-            list.removeChild(e.target.parentElement.parentElement)
+        const thisItem = e.target.parentElement.parentElement;
+        switch (e.target.dataset['type']) {
+            case 'delete' :
+                list.removeChild(thisItem)
+                break;
+            case 'edit' :
+                const input = document.createElement('input');
+                input.setAttribute('type','text');
+                input.classList.add('list__inputEdit');
+                console.log(thisItem.firstElementChild)
+                input.value = thisItem.firstElementChild.textContent;
+                thisItem.replaceChild(input,thisItem.firstElementChild);
+                input.focus();
+                input.addEventListener('keydown',function(e) {
+                    console.log(e)
+                    if(e.keyCode === 13) {
+                        const content = document.createElement('span');
+                        content.classList.add('list__content');
+                        content.textContent = this.value;
+                        thisItem.replaceChild(content,thisItem.firstElementChild);
+                    }
+                })
+                break;
+            case 'check' :
+                let isfinish = e.target.dataset['isfinish'] === '0' ? false : true;
+                if(isfinish) {
+                    list.removeChild(thisItem);
+                    e.target.classList.toggle('fas');
+                    e.target.classList.toggle('far');
+                    e.target.dataset['isfinish'] = '0';
+                    console.log(e.target);
+                    notFinishedList.appendChild(thisItem);
+                }else {
+                    list.removeChild(thisItem);
+                    e.target.classList.toggle('fas');
+                    e.target.classList.toggle('far');
+                    e.target.dataset['isfinish'] = '1';
+                    finishedList.appendChild(thisItem);
+                    console.log(e.target)
+                }
+                break;     
         }
     }
 
@@ -58,6 +100,6 @@
 
 
     addInput.addEventListener('keydown',addItem);
-    notFinishedContainer.addEventListener('click',deleteItem)
-    finishedContainer.addEventListener('click',deleteItem)
+    notFinishedContainer.addEventListener('click',handleItem)
+    finishedContainer.addEventListener('click',handleItem)
 })();
