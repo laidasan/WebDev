0353663929cb6ed11a3a6670b512c313b467bf3b
laidasan <tp6m4xup6tp6@gmail.com>
2020-01-09 15:34:40 +0800

    ToDoList => use localstorage

--- practice2019/41_ToDoList/js/app.js
+++ practice2019/41_ToDoList/js/app.js
@@ -4,10 +4,11 @@
     const finishedContainer = document.querySelector('.finish');
     const notFinishedList = document.querySelector('.notyet .list');
     const finishedList = document.querySelector('.finish .list')
+    const value = localStorage.getItem('value');
 
 
-
-    function ListItem () {
+    //產生一個新的listitem
+    function ListItem() {
         const item = document.createElement('li');
         item.className = 'list__item';
 
@@ -34,53 +35,82 @@
     }
 
 
+    function itemContent() {
+        const span = document.createElement('span');
+        span.classList.add('list__content');
+        return span;
+    }
+
+    //更新localstorage資料
+    function updateLocalStorage() {
+        // 每個Itme裡的sapn.textContent資料存入localStorage
+        const notfinishVlaue = Array.from(document.querySelectorAll('.notyet .list__content'));
+        const finishedVlaue = Array.from(document.querySelectorAll('.finish .list__content'));
+        const notfinsihConent = notfinishVlaue.map((elem) => {
+            return elem.textContent;
+        })
+        const finishedConent = finishedVlaue.map((elem) => {
+            return elem.textContent
+        })
+        const valueObj = {
+            notfinish: notfinsihConent,
+            finished: finishedConent
+        }
+        localStorage.setItem('value', JSON.stringify(valueObj))
+    }
+
+
+   
+
     function addItem(e) {
-        if(this.value.trim() !== '' && e.keyCode === 13) {
-        const content = document.createElement('span')
-        content.className = 'list__content';
-        content.textContent = this.value;
-        const item = new ListItem();
-        item.insertBefore(content,item.firstElementChild);
-        notFinishedContainer.appendChild(item);
-        addInput.value = '';
+        if (this.value.trim() !== '' && e.keyCode === 13) {
+            const content = itemContent();
+            content.textContent = this.value;
+            const item = new ListItem();
+            item.insertBefore(content, item.firstElementChild);
+            notFinishedContainer.appendChild(item);
+            addInput.value = '';
+            updateLocalStorage();
         }
     }
 
-    function handleItem (e) {
+    function handleItem(e) {
         const list = e.target.parentElement.parentElement.parentElement;
         const thisItem = e.target.parentElement.parentElement;
         switch (e.target.dataset['type']) {
-            case 'delete' :
+            case 'delete':
                 list.removeChild(thisItem)
+                updateLocalStorage();
                 break;
-            case 'edit' :
+            case 'edit':
                 const input = document.createElement('input');
-                input.setAttribute('type','text');
+                input.setAttribute('type', 'text');
                 input.classList.add('list__inputEdit');
                 console.log(thisItem.firstElementChild)
                 input.value = thisItem.firstElementChild.textContent;
-                thisItem.replaceChild(input,thisItem.firstElementChild);
+                thisItem.replaceChild(input, thisItem.firstElementChild);
                 input.focus();
-                input.addEventListener('keydown',function(e) {
+                input.addEventListener('keydown', function (e) {
                     console.log(e)
-                    if(e.keyCode === 13) {
+                    if (e.keyCode === 13) {
                         const content = document.createElement('span');
                         content.classList.add('list__content');
                         content.textContent = this.value;
-                        thisItem.replaceChild(content,thisItem.firstElementChild);
+                        thisItem.replaceChild(content, thisItem.firstElementChild);
                     }
                 })
+                updateLocalStorage();
                 break;
-            case 'check' :
+            case 'check':
                 let isfinish = e.target.dataset['isfinish'] === '0' ? false : true;
-                if(isfinish) {
+                if (isfinish) {
                     list.removeChild(thisItem);
                     e.target.classList.toggle('fas');
                     e.target.classList.toggle('far');
                     e.target.dataset['isfinish'] = '0';
                     console.log(e.target);
                     notFinishedList.appendChild(thisItem);
-                }else {
+                } else {
                     list.removeChild(thisItem);
                     e.target.classList.toggle('fas');
                     e.target.classList.toggle('far');
@@ -88,18 +118,48 @@
                     finishedList.appendChild(thisItem);
                     console.log(e.target)
                 }
-                break;     
+                updateLocalStorage();
+                break;
         }
     }
 
 
+     //畫面更新
+    //從localStorage取出資料，畫面更新
+    function render() {
+        const localData = JSON.parse(localStorage.getItem('value'));
+        localData.notfinish.forEach((text) => {
+            const item = ListItem();
+            const contnet = itemContent();
+            contnet.textContent = text;
+            item.insertBefore(contnet,item.firstElementChild);
+            notFinishedList.appendChild(item);
+        })
 
 
+        localData.finished.forEach((text) => {
+            const item = ListItem();
+            const contnet = itemContent();
+            contnet.textContent = text;
+            item.insertBefore(contnet,item.firstElementChild);
+            finishedList.appendChild(item)
+        })
 
+        
+    }
+    window.onload = function() {
+        render();
 
+        //整個畫面載入完成後才顯出ToDoList
+        const ToDoList = document.querySelector('.ToDoList');
+        const title = document.querySelector('.title');
+        ToDoList.classList.toggle('hidden');
+        title.classList.toggle('hidden');
 
+        //添加監聽事件
+        addInput.addEventListener('keydown', addItem);
+        notFinishedContainer.addEventListener('click', handleItem);
+        finishedContainer.addEventListener('click', handleItem);
+    }
 
-    addInput.addEventListener('keydown',addItem);
-    notFinishedContainer.addEventListener('click',handleItem)
-    finishedContainer.addEventListener('click',handleItem)
 })();
