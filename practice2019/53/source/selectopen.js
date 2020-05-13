0126387bd48cbb38906bf5f9fe8e32c4c0a1a974
laidasan <tp6m4xup6tp6@gmail.com>
2020-05-13 20:59:52 +0800

    sign page

--- practice2019/53/source/selectopen.js
+++ practice2019/53/source/selectopen.js
@@ -1,51 +1,78 @@
 ; (() => {
-    const $loginForget = document.querySelector('.login--forget')
-    const $emailList = document.querySelector('.emailList')
+    const $login = document.querySelector('.login')
+    // const $selectList = document.querySelector('.selectList')
+    // console.log($login,$selectList)
 
 
     function listClickHandler(target) {
-        console.log('List Click')
-        console.log(target)
+        // change select value and open or close selectList
         target.parentElement.previousElementSibling.textContent = target.textContent
         target.parentElement.classList.toggle('open')
         target.parentElement.parentElement.classList.toggle('open')
+
+        if(target.parentElement.className.includes('cityList')) {
+            let locationList = document.querySelector('.locationList')
+            locationList.innerHTML = ''
+            let $city = document.querySelector('.city')
+           
+            let xhr = new XMLHttpRequest
+            xhr.open('Get','../data/location.json')
+            xhr.onload = function () {
+                let data = JSON.parse(xhr.responseText)
+                let index = data['city'].findIndex(cityName => cityName === $city.textContent)
+                data['region'][index].forEach(location => {
+                     // create list item
+                    let listItem = document.createElement('li')
+                    listItem.className = 'login__selectGroup__listItem'
+                    listItem.textContent = location
+                    // append item to list
+                    locationList.appendChild(listItem)
+                })
+            }
+            xhr.send()
+        }
     }
 
-    function emailClickHandler(target) {
-        console.log('Email Span Click')
+    function selectClickHandler(target) {
+        // console.log('select Span Click')
+        // open or close selestList
+        let $selectList = target.nextElementSibling
         target.parentElement.classList.toggle('open')
-        $emailList.classList.toggle('open')
+        $selectList.classList.toggle('open')
+
+        
+        
     }
 
     function submit(target) {
-        
+        return this
     }
 
 
     function loginClickHandler(e) {
+        console.log('click')
         let target = e.target
         let active = ''
 
-        // if(target.matches('.email')) {
-        //     active = 'email'
-        // }else if(target.matches('.emailList')) {
-        //     active = 'emailList'
-        // }else if(target.matches('.login__btn--forget')) {
-        //     active = 'submit'
-        // }
+        // select => click p tag , open or close select window
+        // selectList => after open select window , select email
+        // login__btn => form submit button
 
-        // email => click p tag , open or close select window
-        // emailList => after open select window , select email
-        // login__bnt--forget => form submit button
-        target.className.includes('email') ? active = 'email' : ''
-        target.parentElement.className.includes('emailList') ? active = 'emailList' : ''
-        target.className.includes('login__btn--forget') ? active = 'submit' : ''
+        new RegExp(/select$/).test(target.className) ? active = 'select' : ''
+        // target.matches('.select') ? active = 'select' : ''
+
+        target.parentElement.className.includes('selectList') ? active = 'selectList' : ''
+        target.className.includes('login__btn') ? active = 'submit' : ''
+        console.log(active)
 
         switch (active) {
-            case 'email':
-                emailClickHandler(target)
+            // case 'email':
+            case 'select':
+                console.log(target)
+                selectClickHandler(target)
                 break;
-            case 'emailList':
+            // case 'emailList':
+            case 'selectList':
                 listClickHandler(target)
                 break;
             case 'submit':
@@ -58,7 +85,7 @@
 
 
     function loadEvent() {
-        $loginForget.addEventListener('click', loginClickHandler)
+        $login.addEventListener('click', loginClickHandler)
     }
 
     loadEvent()
